edded779e696e54e847a5cd23f174a60aabf24e7
Anuraag (Rag) Agrawal <anuraaga@gmail.com>
2024-10-11 10:27:37 +0900

    Update golangci-lint

--- internal/versions/versions.go
+++ internal/versions/versions.go
@@ -2,7 +2,7 @@ package versions
 
 const (
 	GolangPerf   = "v0.0.0-20230906145419-e9dd5d2713ed"
-	GolangCILint = "v1.54.2"
+	GolangCILint = "v1.61.0"
 	GosImports   = "v0.3.8"
 	GoFumpt      = "v0.5.0"
 )
