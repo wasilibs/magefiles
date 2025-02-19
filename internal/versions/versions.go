1485a49a5a4615d13fa893af5a32080a015680c5
Anuraag Agrawal <anuraaga@gmail.com>
2025-02-19 14:12:27 +0900

    Update tools

--- internal/versions/versions.go
+++ internal/versions/versions.go
@@ -2,7 +2,7 @@ package versions
 
 const (
 	GolangPerf   = "v0.0.0-20230906145419-e9dd5d2713ed"
-	GolangCILint = "v1.61.0"
+	GolangCILint = "v1.64.5"
 	GosImports   = "v0.3.8"
-	GoFumpt      = "v0.5.0"
+	GoFumpt      = "v0.7.0"
 )
